7a734f37547b987074c2c186152f0a44d53f99cf
DESKTOP-LU9GGEO\kimminhyeon <wsmh.kim12@gmail.com>
2025-08-15 13:45:49 +0900

    feat : spring 프로젝트 생성

---
+++ source/back-end/study-buddy/src/main/java/com/hexa/curriculum/domain/Curriculum.java
@@ -0,0 +1,12 @@
+package com.hexa.curriculum.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Curriculum {
+
+    @Id
+    private int id;
+ 
+}
