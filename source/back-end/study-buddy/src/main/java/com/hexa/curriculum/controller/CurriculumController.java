8ee0012aacf6845c45ee5e3cbe29d90320850a9e
DESKTOP-LU9GGEO\kimminhyeon <wsmh.kim12@gmail.com>
2025-08-17 08:48:15 +0900

    Feat : github actions 수정 , controller 추가

---
+++ source/back-end/study-buddy/src/main/java/com/hexa/curriculum/controller/CurriculumController.java
@@ -0,0 +1,16 @@
+package com.hexa.curriculum.controller;
+
+import lombok.Getter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class CurriculumController {
+
+    @GetMapping("")
+    public String getCurriculum() {
+        return "Curriculum";
+    }
+}
